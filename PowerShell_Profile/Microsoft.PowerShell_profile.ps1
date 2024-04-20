fe2e1ea0f471cff5474924731772d35483d20440
HashingTuna <38752407+Hrztrm@users.noreply.github.com>
2024-04-20 21:45:21 +0800

    Update Microsoft.PowerShell_profile.ps1

--- PowerShell_Profile/Microsoft.PowerShell_profile.ps1
+++ PowerShell_Profile/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,8 @@
 # To edit your PowerShell profile, open PowerShell and run
 # notepad $PROFILE
 
-
+# PowerShell Alias is a shortcut for a cmdlet or command, while Function is a shortcut for a script or a series of command
+# https://www.sharepointdiary.com/2021/08/powershell-alias-beginners-guide.html#:~:text=In%20summary%2C%20PowerShell%20Alias%20is,or%20a%20series%20of%20commands.
 Set-Alias -Name adb -Value "$env:USERPROFILE\AppData\Local\Android\Sdk\platform-tools\adb.exe"
 
 function vol2{
@@ -36,5 +37,6 @@ function 64g++{
     & "$env:USERPROFILE\Tools\mingw64\bin\g++.exe" $args
 }
 
-
+# Am using oh-my-posh
+# https://ohmyposh.dev/docs/installation/windows
 oh-my-posh init pwsh --config "$env:USERPROFILE\AppData\Local\Programs\oh-my-posh\themes\tokyo.omp.json"| Invoke-Expression
